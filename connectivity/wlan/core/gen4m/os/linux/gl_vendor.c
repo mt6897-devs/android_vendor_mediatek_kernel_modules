bcb99a4ba4f5f04befa605a0d9bda534116adcab
Ben Lai <ben.lai@mediatek.com>
2023-11-28 10:03:11 +0800

    [ALPS08545012] wlan: Support AOSP concurrency matrix
    Support AOSP concurrency matrix.
    
    MTK-Commit-Id: d6a3b8578e6b022e9652613ba9e141b51d0033af
    
    CR-Id: ALPS08545012
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    Feature: Wi-Fi Driver
    Change-Id: I8717377c9bad79b372f0d92d3551755eb924acbd
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8004463
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: chris.hong <chris.hong@mediatek.com>
    Reviewed-by: johnny.chiang <johnny.chiang@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Test: srv_mspautosanity
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8002389
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>

--- connectivity/wlan/core/gen4m/os/linux/gl_vendor.c
+++ connectivity/wlan/core/gen4m/os/linux/gl_vendor.c
@@ -3901,6 +3901,231 @@ nla_put_failure:
 	return -EINVAL;
 }
 
+struct wifi_iface_limit sta_sta[] = {
+	{
+		.max_limit = 2,
+		.iface_mask = BIT(WIFI_INTERFACE_TYPE_STA),
+	},
+};
+
+struct wifi_iface_limit sta_ap[] = {
+	{
+		.max_limit = 1,
+		.iface_mask = BIT(WIFI_INTERFACE_TYPE_STA),
+	},
+	{
+		.max_limit = 1,
+		.iface_mask = BIT(WIFI_INTERFACE_TYPE_AP),
+	},
+};
+
+struct wifi_iface_limit sta_p2p[] = {
+	{
+		.max_limit = 1,
+		.iface_mask = BIT(WIFI_INTERFACE_TYPE_STA),
+	},
+	{
+		.max_limit = 1,
+		.iface_mask = BIT(WIFI_INTERFACE_TYPE_P2P),
+	},
+};
+
+struct wifi_iface_limit sta_nan[] = {
+	{
+		.max_limit = 1,
+		.iface_mask = BIT(WIFI_INTERFACE_TYPE_STA),
+	},
+	{
+		.max_limit = 1,
+		.iface_mask = BIT(WIFI_INTERFACE_TYPE_NAN),
+	},
+};
+
+struct wifi_iface_limit ap_ap[] = {
+	{
+		.max_limit = 1,
+		.iface_mask = BIT(WIFI_INTERFACE_TYPE_AP_BRIDGED),
+	},
+};
+
+struct mtk_wifi_iface_combination mtk_ifaces_combinations[] = {
+	{
+		.max_ifaces = 2,
+		.num_iface_limits = ARRAY_SIZE(sta_sta),
+		.iface_limits = sta_sta,
+	},
+	{
+		.max_ifaces = 1,
+		.num_iface_limits = ARRAY_SIZE(ap_ap),
+		.iface_limits = ap_ap,
+	},
+	{
+		.max_ifaces = 2,
+		.num_iface_limits = ARRAY_SIZE(sta_ap),
+		.iface_limits = sta_ap,
+	},
+	{
+		.max_ifaces = 2,
+		.num_iface_limits = ARRAY_SIZE(sta_p2p),
+		.iface_limits = sta_p2p,
+	},
+	{
+		.max_ifaces = 2,
+		.num_iface_limits = ARRAY_SIZE(sta_nan),
+		.iface_limits = sta_nan,
+	},
+};
+
+struct mtk_wifi_iface_combination mtk_ifaces_combinations_6631[] = {
+	{
+		.max_ifaces = 1,
+		.num_iface_limits = ARRAY_SIZE(ap_ap),
+		.iface_limits = ap_ap,
+	},
+	{
+		.max_ifaces = 2,
+		.num_iface_limits = ARRAY_SIZE(sta_ap),
+		.iface_limits = sta_ap,
+	},
+	{
+		.max_ifaces = 2,
+		.num_iface_limits = ARRAY_SIZE(sta_p2p),
+		.iface_limits = sta_p2p,
+	},
+	{
+		.max_ifaces = 2,
+		.num_iface_limits = ARRAY_SIZE(sta_nan),
+		.iface_limits = sta_nan,
+	},
+};
+
+struct mtk_wifi_iface_concurrency_matrix mtk_ifaces_matrix = {
+	.num_iface_combinations = ARRAY_SIZE(mtk_ifaces_combinations),
+	.iface_combinations = mtk_ifaces_combinations,
+};
+
+struct mtk_wifi_iface_concurrency_matrix mtk_ifaces_matrix_6631 = {
+	.num_iface_combinations = ARRAY_SIZE(mtk_ifaces_combinations_6631),
+	.iface_combinations = mtk_ifaces_combinations_6631,
+};
+
+int mtk_cfg80211_vendor_get_chip_concurrency_matrix(struct wiphy *wiphy,
+		struct wireless_dev *wdev, const void *data, int data_len)
+{
+#define DEBUG_BUFFER_SZ		512
+
+	struct mtk_wifi_iface_concurrency_matrix *src = NULL;
+	struct wifi_iface_concurrency_matrix *dest = NULL;
+	struct GLUE_INFO *prGlueInfo = NULL;
+	struct mt66xx_chip_info *prChipInfo = NULL;
+	struct sk_buff *skb;
+	uint8_t i, j;
+	uint8_t buffer[DEBUG_BUFFER_SZ];
+	int32_t written = 0;
+
+	if (!wiphy || !wdev) {
+		DBGLOG(REQ, ERROR,
+			"wiphy=0x%p, wdev=0x%p\n",
+			wiphy, wdev);
+		return -EINVAL;
+	}
+
+	skb = cfg80211_vendor_cmd_alloc_reply_skb(wiphy, sizeof(*dest));
+
+	if (!skb) {
+		DBGLOG(REQ, ERROR,
+			"cfg80211_vendor_cmd_alloc_reply_skb failed(%zu)\n",
+			sizeof(*dest));
+		goto nla_put_failure;
+	}
+
+	dest = (struct wifi_iface_concurrency_matrix *)
+		kalMemAlloc(sizeof(*dest), VIR_MEM_TYPE);
+	if (!dest) {
+		DBGLOG(REQ, ERROR,
+			"alloc matrix failed(%zu)\n",
+			sizeof(*dest));
+		goto nla_put_failure;
+	}
+	kalMemZero(dest, sizeof(*dest));
+	kalMemZero(buffer, sizeof(buffer));
+
+	prGlueInfo = wlanGetGlueInfo();
+	if (!prGlueInfo) {
+		DBGLOG(REQ, ERROR, "get glue structure fail.\n");
+		goto nla_put_failure;
+	}
+
+	prChipInfo = prGlueInfo->prAdapter->chip_info;
+	if (!prChipInfo) {
+		DBGLOG(REQ, ERROR, "prChipInfo=0x%p\n",	prChipInfo);
+		goto nla_put_failure;
+	}
+
+	if (prChipInfo->asicGetChipID &&
+	    prChipInfo->asicGetChipID(prGlueInfo->prAdapter) == 0x31)
+		src = &mtk_ifaces_matrix_6631;
+	else
+		src = &mtk_ifaces_matrix;
+
+	dest->num_iface_combinations = src->num_iface_combinations;
+	written += kalSnprintf(buffer + written,
+			       sizeof(buffer) - written,
+			       "combinations=%u\n",
+			       dest->num_iface_combinations);
+	for (i = 0; i < src->num_iface_combinations; i++) {
+		struct mtk_wifi_iface_combination *src_comb =
+			&src->iface_combinations[i];
+		struct wifi_iface_combination *dest_comb =
+			&dest->iface_combinations[i];
+
+		dest_comb->max_ifaces = src_comb->max_ifaces;
+		dest_comb->num_iface_limits = src_comb->num_iface_limits;
+		written += kalSnprintf(buffer + written,
+				       sizeof(buffer) - written,
+				       "\t[%u] max=%u limits=%u,",
+				       i,
+				       dest_comb->max_ifaces,
+				       dest_comb->num_iface_limits);
+		for (j = 0; j < src_comb->num_iface_limits; j++) {
+			struct wifi_iface_limit *src_limit =
+				&src_comb->iface_limits[j];
+			struct wifi_iface_limit *dest_limit =
+				&dest_comb->iface_limits[j];
+
+			dest_limit->max_limit = src_limit->max_limit;
+			dest_limit->iface_mask = src_limit->iface_mask;
+			written += kalSnprintf(buffer + written,
+					       sizeof(buffer) - written,
+					       " [%u, max=%u mask=%u]",
+					       j,
+					       dest_limit->max_limit,
+					       dest_limit->iface_mask);
+		}
+		written += kalSnprintf(buffer + written,
+				       sizeof(buffer) - written,
+				       "\n");
+	}
+	DBGLOG(REQ, INFO, "%s", buffer);
+
+	if (unlikely(nla_put(skb, WIFI_ATTRIBUTE_CONCURRENCY_MATRIX,
+			      sizeof(*dest), (void *)dest) < 0))
+		goto nla_put_failure;
+
+	kalMemFree(dest, VIR_MEM_TYPE, sizeof(*dest));
+
+	return cfg80211_vendor_cmd_reply(skb);
+
+nla_put_failure:
+	if (skb)
+		kfree_skb(skb);
+
+	if (dest)
+		kalMemFree(dest, VIR_MEM_TYPE, sizeof(*dest));
+
+	return -EINVAL;
+}
+
 int mtk_cfg80211_vendor_get_apf_capabilities(struct wiphy *wiphy,
 	struct wireless_dev *wdev, const void *data, int data_len)
 {
