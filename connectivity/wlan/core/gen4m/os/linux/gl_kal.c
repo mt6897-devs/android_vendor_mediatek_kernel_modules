28e7e13507f93fe50b12834ab248ba6aea4567b8
bengris32 <bengris32@protonmail.ch>
2024-03-07 21:01:19 +0000

    connectivity: gen4m: Don't block suspend if WLAN is halted
    * In gen4m's PM notifier, we return NOTIFY_STOP if the WLAN is suspended,
      this covers conditions such as where the driver isn't ready yet OR
      if WLAN has been turned off.
    
    * Returning NOTIFY_STOP in a PM notifier will block suspend completely,
      blocking suspend when WLAN is off isn't exactly desirable behaviour,
      so just return NOTIFY_DONE to ensure suspend can work normally even
      when WLAN is turned off.
    
    Change-Id: I498b47aa77132912c1a84cfb88c35d3b64fa4db9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- connectivity/wlan/core/gen4m/os/linux/gl_kal.c
+++ connectivity/wlan/core/gen4m/os/linux/gl_kal.c
@@ -11585,8 +11585,7 @@ static int wlan_pm_notifier_callback(struct notifier_block
 		return NOTIFY_STOP;
 
 	if (kalIsHalted() || !prGlueInfo) {
-		kalHaltUnlock();
-		return NOTIFY_STOP;
+		goto out;
 	}
 
 	switch (event) {
