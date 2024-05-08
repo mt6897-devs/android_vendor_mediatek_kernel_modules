3c9a79b15b0ce073a7ec1e228a37fe7923f1514c
wangtong.wang <wangtong.wang@mediatek.com>
2024-05-09 01:52:22 +0800

    [ALPS08927865] Fix:Dos attack issues
    [Description]
    Fix: Dos attack issues
    
    [Test]
    RD UTIT/ Preflight/ HST
    
    MTK-Commit-Id: 396aca906983acc4f891c04c022ee9c4b756cc8e
    
    Feature: [Module]Wi-Fi Driver
    Change-Id: I6f9e2939fe29f87a9adfb303c194b53fae52054d
    CR-Id: ALPS08927865
    Signed-off-by: wangtong.wang <wangtong.wang@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8999854
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: johnny.chiang <johnny.chiang@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Tested-by: johnny.chiang <johnny.chiang@mediatek.com>
    (cherry picked from commit 0b323d7bb4b1c1bef31037cb2437b2c503828ac7)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9025207

--- connectivity/wlan/core/gen4m/mgmt/assoc.c
+++ connectivity/wlan/core/gen4m/mgmt/assoc.c
@@ -1635,7 +1635,16 @@ uint32_t assocProcessRxAssocReqFrameImpl(
 			}
 			break;
 #endif
-
+		case ELEM_ID_SUP_CHS:
+			if ((IE_LEN(pucIE) >
+				ELEM_MAX_LEN_SUPPORTED_CHANNELS)
+				|| (IE_LEN(pucIE) & 0x01))
+				return WLAN_STATUS_FAILURE;
+			break;
+		case ELEM_ID_PWR_CAP:
+			if (IE_LEN(pucIE) != ELEM_MAX_LEN_POWER_CAP)
+				return WLAN_STATUS_FAILURE;
+			break;
 		case ELEM_ID_EXTENDED_SUP_RATES:
 			if (!prIeExtSupportedRate)
 				prIeExtSupportedRate = EXT_SUP_RATES_IE(pucIE);
