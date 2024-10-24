e2a2a898a677c0e3b60d82e9f331ad9b72fa49fd
Jake Chung <jake.chung@mediatek.com>
2024-10-24 19:57:44 +0800

    WFSYS: wlanRemove after wlanOnAtReset fail
    [Description]
    wlanRemove after wlanOnAtReset fail
    
    [Test]
    build pass, L0/L0.5 UT pass
    
    Change-Id: Iae88ff10a70d150b5396782be67ab231d5fab987
    Mot-CRs-Fixed: (CR)
    CR-Id: ALPS09855577
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Jake Chung <jake.chung@mediatek.com>
    Signed-off-by: Yue Sun <sunyue5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3329653
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhilu Yin <yinzl1@motorola.com>
    Submit-Approved: Jira Key

--- connectivity/wlan/core/gen4m/os/linux/gl_init.c
+++ connectivity/wlan/core/gen4m/os/linux/gl_init.c
@@ -2146,6 +2146,8 @@ static const struct wiphy_wowlan_support mtk_wlan_wowlan_support = {
  *******************************************************************************
  */
 
+static void wlanRemove(void);
+
 /*******************************************************************************
  *                              F U N C T I O N S
  *******************************************************************************
@@ -7232,6 +7234,9 @@ int32_t wlanOnAtReset(void)
 		 * If WMT being removed in the future, you should invoke
 		 * wlanRemove directly from here
 		 */
+		kalSendAeeWarning("WFSYS", "wlanOnAtReset fail\n");
+		wlanRemove();
+
 #if 0
 		switch (eFailReason) {
 		case ADAPTER_START_FAIL:
@@ -7689,7 +7694,7 @@ wlanOffNotifyCfg80211Disconnect(struct GLUE_INFO *prGlueInfo)
  * \return (none)
  */
 /*----------------------------------------------------------------------------*/
-static void wlanRemove(void)
+void wlanRemove(void)
 {
 	struct net_device *prDev = NULL;
 	struct NETDEV_PRIVATE_GLUE_INFO *prNetDevPrivate = NULL;
