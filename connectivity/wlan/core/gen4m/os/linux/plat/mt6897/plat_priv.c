6d930190159a3af61b7882cc545e08631001afc4
Mashopy <eliasgheeraert@gmail.com>
2025-07-10 19:36:11 +0200

    connectivity: gen4m: Disable MTK RPS
    Change-Id: I5059ce3d30583e54bd0b69a4040ba8fc2a6fe041

--- connectivity/wlan/core/gen4m/os/linux/plat/mt6897/plat_priv.c
+++ connectivity/wlan/core/gen4m/os/linux/plat/mt6897/plat_priv.c
@@ -229,7 +229,9 @@ int32_t kalBoostCpu(struct ADAPTER *prAdapter,
 
 	if (fgRequested == ENUM_CPU_BOOST_STATUS_INIT) {
 		/* initially enable rps working at small cores */
+#ifdef CONFIG_MTK_NET_RPS
 		kalSetRpsMap(prGlueInfo, RPS_LITTLE_CORE);
+#endif
 		kalSetISRMask(prAdapter, CPU_LITTLE_CORE);
 		fgRequested = ENUM_CPU_BOOST_STATUS_STOP;
 	}
@@ -250,7 +252,9 @@ int32_t kalBoostCpu(struct ADAPTER *prAdapter,
 		kalSetCpuMask(prGlueInfo->napi_thread, CPU_ALL_CORE);
 #endif
 
+#ifdef CONFIG_MTK_NET_RPS
 		kalSetRpsMap(prGlueInfo, RPS_LITTLE_CORE);
+#endif
 		kalSetISRMask(prAdapter, CPU_ALL_CORE);
 		kalSetDramBoost(prAdapter, TRUE);
 	} else if (u4TarPerfLevel >= u4BoostCpuTh &&
@@ -278,7 +282,9 @@ int32_t kalBoostCpu(struct ADAPTER *prAdapter,
 		kalSetCpuMask(prGlueInfo->napi_thread, CPU_BIG_CORE);
 		kalSetTaskUtilMinPct(prGlueInfo->u4RxNapiThreadPid, 100);
 #endif
+#ifdef CONFIG_MTK_NET_RPS
 		kalSetRpsMap(prGlueInfo, RPS_BIG_CORE);
+#endif
 		kalSetISRMask(prAdapter, CPU_BIG_CORE);
 		kalSetDramBoost(prAdapter, TRUE);
 #if CFG_SUPPORT_LITTLE_CPU_BOOST
@@ -302,7 +308,9 @@ int32_t kalBoostCpu(struct ADAPTER *prAdapter,
 		kalSetTaskUtilMinPct(prGlueInfo->u4RxNapiThreadPid, 0);
 #endif
 
+#ifdef CONFIG_MTK_NET_RPS
 		kalSetRpsMap(prGlueInfo, RPS_LITTLE_CORE);
+#endif
 		kalSetISRMask(prAdapter, CPU_LITTLE_CORE);
 		kalSetCpuFreq(DEFAULT_CPU_FREQ, CPU_BIG_CORE);
 	} else if (u4TarPerfLevel < u4BoostLittleCpuTh &&
@@ -334,7 +342,9 @@ int32_t kalBoostCpu(struct ADAPTER *prAdapter,
 		kalSetCpuMask(prGlueInfo->napi_thread, CPU_ALL_CORE);
 		kalSetTaskUtilMinPct(prGlueInfo->u4RxNapiThreadPid, 0);
 #endif
+#ifdef CONFIG_MTK_NET_RPS
 		kalSetRpsMap(prGlueInfo, RPS_LITTLE_CORE);
+#endif
 		kalSetISRMask(prAdapter, CPU_ALL_CORE);
 		kalSetCpuFreq(DEFAULT_CPU_FREQ, CPU_ALL_CORE);
 		kalSetDramBoost(prAdapter, FALSE);
